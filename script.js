6f759e706afe9e25da792d4b9638f3952f156e59
Kevin Herrera <1herrerake@gmail.com>
2016-10-14 19:26:34 -0400

    Began on the JS. Set the initial vvariables and also tested them for effectiveness.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 
+var one = $("#one")
+var two = $("#two")
+var three = $("#three")
+
+if (1<2){
+  console.log("T");
+}
+
+
+
+var game = {
+  left:[one,two,three],
+  middle:[],
+  right:[]
+}
 
 });
