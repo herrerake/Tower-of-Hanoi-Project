0d14edfc222863adc357afc1d0d81c5cfafc5bcc
Kevin Herrera <1herrerake@gmail.com>
2016-10-17 12:41:10 -0400

    The rings are functional. Next move would be to apply the rules.

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 $(document).ready(function(){
 
+//GLOBAL VARIABLES - THE RINGS
 var one = $("#one")
 var two = $("#two")
 var three = $("#three")
+var left = $("#left")
+var middle = $("#middle")
+var right = $("#right")
+var columns = $(".columns")
+var rings = $(".rings")
 
-if (1<2){
-  console.log("T");
-}
 
 
-
+//THE COLUMNS
 var game = {
   left:[one,two,three],
   middle:[],
   right:[]
 }
 
+//Functionality Code - MOVING THE RINGS
+
+columns.on("click", function(){
+
+  if(columns.hasClass("select")) {
+
+  topRing = $(this).find(".rings").first()
+  topRing.css("border", "4px solid black")
+  columns.addClass("move").removeClass("select")
+
+} else if (columns.hasClass("move")) {
+    topRing.css("border", "none")
+    columns.addClass("select").removeClass("move")
+      $(this).prepend(topRing)
+    }
+
+})
+
+
 });
+
+
+
+
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
+//
+// function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+// }
+//
+// function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+// }
