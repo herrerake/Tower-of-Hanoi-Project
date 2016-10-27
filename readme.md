c2343215964e5b70bc04287c2660b59c5e5548d0
James Reichard <jpqreichard@gmail.com>
2016-10-27 13:45:11 -0400

    add james' comments

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+<!-- check out the link below for tips writing in markdown! it's like playskool HTML and is super easy to write in! -->
+<!-- https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet -->
 Bronze Goals
 --For this prize I will be focusing on the format, layout, and organization of the html such as deciding which tags, classes, and IDs to use to construct the rings and pegs of the tower.
 --With the user in mind I am visualizing how the tower containing all the rings look and also how the two other empty pegs look.
@@ -31,4 +33,4 @@ Gold Goals
 --Thinking about having an opening sequence to the game where the user clicks start and the pegs and tower phase in.
 --I want to display in the background a high quality picture of the city of Hanoi.
 --A move counter where the user can see how many moves it took to complete.
---A prompt that will notify the user when the game has been won. 
+--A prompt that will notify the user when the game has been won.
--- script.js
+++ script.js
@@ -1,52 +1,63 @@
 $(document).ready(function(){
 
-//GLOBAL VARIABLES - THE RINGS & COLUMNS
-var one = $("#one")
-var two = $("#two")
-var three = $("#three")
-var four = $("#four")
-var five = $("#five")
-var left = $("#left")
-var middle = $("#middle")
-var right = $("#right")
-var columns = $(".columns")
-var rings = $(".rings")
+  // Nice work commenting your code! also, just as a general note, this is some of the cleanest tower of hanoi code I've seen in my short time as an instructor. super readable and well-organized
 
 
-//Functionality Code - MOVING THE RINGS
-var global = 0;
-columns.on("click", function(){
-  //This solves the problem of setting rings with larger values on top of smaller ones.
-  //By establishing a value on the columns based upon the value of the top ring. Initially
-  //I established a global value of 0 on the onset then by doing global++ the global value
-  //will not go back to 0.
-  if (global = 0){
-  topRingValue = 0;
-  global++;
-}
-//By giving the columns a class of select and move I am now able to toggle
-//between the two actions. When in class move and clicked on a new column
-//then the ring will append.
-  if(columns.hasClass("select")) {
-    topRing = $(this).find(".rings").first()
-    topRingValue = topRing.attr("value");
-    console.log(topRingValue)
-    topRing.css("border", "4px solid black");
-    columns.addClass("move").removeClass("select");
-}
-  else if (columns.hasClass("move")) {
-    console.log(topRingValue);
-    console.log($(this).find(".rings").first().attr("value"));
+  //GLOBAL VARIABLES - THE RINGS & COLUMNS
+  var one = $("#one")
+  var two = $("#two")
+  var three = $("#three")
+  var four = $("#four")
+  var five = $("#five")
+  var left = $("#left")
+  var middle = $("#middle")
+  var right = $("#right")
+  var columns = $(".columns")
+  var rings = $(".rings")
 
 
-    //There is a shadow ring of 99 that is used as a placeholder so that any ring
-    //can sit on top. Solving the issue when a column is empty.
-  if ($(this).find(".rings").first().attr("value") > topRingValue){
-    topRing.css("border", "none");
-    columns.addClass("select").removeClass("move");
-      $(this).prepend(topRing);
-      console.log(topRingValue);
+  //Functionality Code - MOVING THE RINGS
+
+  //is global # of moves? is it supposed to track whether or not the game has started?
+  var global = 0;
+  columns.on("click", function(){
+    //This solves the problem of setting rings with larger values on top of smaller ones.
+    //By establishing a value on the columns based upon the value of the top ring. Initially
+    //I established a global value of 0 on the onset then by doing global++ the global value
+    //will not go back to 0.
+
+    //careful! use == for comparison
+    if (global = 0){
+      topRingValue = 0;
+      global++;
+    }
+    //By giving the columns a class of select and move I am now able to toggle
+    //between the two actions. When in class move and clicked on a new column
+    //then the ring will append.
+
+    // I really like how clean and readable your code is here
+    if(columns.hasClass("select")) {
+      //I'd use var or let here so that topRing isn't a global variable
+      topRing = $(this).find(".rings").first()
+      topRingValue = topRing.attr("value");
+      // I might make the select class include the styling rule below, and then you can jsut toggle it using toggleClass
+      topRing.css("border", "4px solid black");
+      // I'd use toggleClass here for select, I think this would potentially eliminate the need for the move class as well!
+      columns.addClass("move").removeClass("select");
+    } else if ( columns.hasClass("move") ) {
+
+      //There is a shadow ring of 99 that is used as a placeholder so that any ring
+      //can sit on top. Solving the issue when a column is empty.
+
+      // I'm not convinced you need the shadow ring, but perhaps I was just unable to reproduce the bug.
+      // That being said, great practice to document a workaround! Your commenting practices are exemplary and commendable
+      if ($(this).find(".rings").first().attr("value") > topRingValue){
+        topRing.css("border", "none");
+        columns.addClass("select").removeClass("move");
+        $(this).prepend(topRing);
+        console.log(topRingValue);
       }
     }
-})
+    //I'd add a checkWin each time a legal move is made, then your game would be complete!
+  })
 });
